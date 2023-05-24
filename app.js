7bb0e46000c5c52d8c2ee8f06be55b3a50797d74
unknown <asifs.cse@gmail.com>
2023-05-24 23:51:50 +0530

    database connect successfully

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const dbConnect = require('./config/database');
 const app = express();
 
+dbConnect();
 
 //set view engine pug
 app.set('view engine','pug');
@@ -10,8 +12,23 @@ app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+//store sesson in mongodb
+
+
+//create route
+//base url
 app.get('/',(req, res)=>{
   res.render('index');
 })
+
+//register get route
+app.get('/regiser', (req, res)=>{
+  res.render('register');
+})
+//register post route
+app.post('/register',(req, res)=>{
+  
+})
+
 
 module.exports = app;
---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const dbConnect = async ()=>{
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/SessionApp');
+        console.log('database connection successfull');
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+module.exports = dbConnect;
