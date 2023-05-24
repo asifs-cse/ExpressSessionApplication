121e5818c60459608257ec7d67dea5ad9dd2b61f
unknown <asifs.cse@gmail.com>
2023-05-24 22:07:39 +0530

    set view engine pug

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
+
+//set view engine pug
+app.set('view engine','pug');
+app.set('views','./views/');
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/',(req, res)=>{
+  res.render('index');
+})
 
 module.exports = app;
