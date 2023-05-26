4684edd7d44e4fb67d5ccf0130cc98a8792942ba
unknown <asifs.cse@gmail.com>
2023-05-26 23:51:47 +0530

    database

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 const dbConnect = require('./config/database');
+require('./config/database');
 const userModel = require('./models/user.model');
 const app = express();
 
 dbConnect();
+
+//create session using mongodb
+const mongoStore = require('connect-mongo');
+const passport = require('passport');
+const session = require('express-session');
+
+//create session
+app.set('trust proxy',1);
+app.use(
+  session({
+    secret: 'keybord cat',
+    resave: false,
+    saveUninitialized: true,
+    store: mongoStore.create({
+      mongoUrl: 'mongodb://127.0.0.1:27017/passportAuthUser',
+      collectionName: 'sessions',
+    })
+  })
+)
+
+//initialize passport and session
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 //set view engine pug
 app.set('view engine','pug');
@@ -12,9 +38,6 @@ app.set('views','./views/');
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
-//store sesson in mongodb
-
 
 //create route
 //base url
@@ -51,19 +74,49 @@ app.post('/register',async (req, res)=>{
   }
 });
 
+//check login
+const checkLoggedIn = (req, res, next)=>{
+  if(req.isAuthenticated()){
+    return res.redirect('/profile');
+  }
+  next();
+}
 //login get route
-app.get('/login', async (req, res)=>{
+app.get('/login', checkLoggedIn, (req, res)=>{
   res.render('login');
+})
+
+//login post route
+app.post('/login', passport.authenticate('local', {failureRedirect: '/login', successRedirect: '/profile'}),
+function(req, res){
+  res.redirect('/');
+})
+
+//protected route
+const checkAuthed = (req, res)=>{
+  if(req.isAuthenticated()){
+    return next();
+  }else{
+    res.redirect('/login');
+  }
+}
+//profile route
+app.get('/profile', checkAuthed, (req, res)=>{
+  res.send('/profile')
 })
 
 //logout route
 app.get('/logout', async (req, res)=>{
-  res.send('logout');
-})
-
-//profile route
-app.get('/profile', async (req, res)=>{
-  res.send('profile')
+  try {
+    req.logout((err)=>{
+      if(err){
+        return next(err);
+      }
+      res.redirect('/');
+    })
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 })
 
 module.exports = app;
